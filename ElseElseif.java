f88ad4a1c5f4f81b6ec6ff9a15074680c9733e2a
phlc <pedrocarvalho@outlook.com>
2020-01-07 14:54:58 -0300

    combining statements

--- ElseElseif.java
+++ ElseElseif.java
@@ -4,6 +4,7 @@ public class ElseElseif{
     int x = 3;
     double d = 2.0;
 
+//test with else if
     if (y < 3)
       System.out.println("Ok.");
     else if (y < 6)
@@ -11,11 +12,25 @@ public class ElseElseif{
     else
       System.out.println("Nao deve mostrar - 8.");
 
+//test without else if
     if (y < 3)
       System.out.println("Ok.");
     if (y < 6)
       System.out.println("Deve mostrar -  Sem else if.");
     else
         System.out.println("Nao deve mostrar - 8.");
+
+//test combining if statements
+    if (y < 4){
+      System.out.println("Ok test");
+      if (y == d){
+        System.out.println("int == double");
+      }
+    }
+
+    if (y<4 && y==d)
+      System.out.println("Same as before, using &&");
+
+    
   }
 }
